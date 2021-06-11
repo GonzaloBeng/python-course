cc6170b4ac6f8c6626fa01643c453dc56ee097d6
Gonzalo Beng <gonzalobeng@gmail.com>
2021-06-10 23:57:06 -0300

    Tipos de variables y strings

---
+++ strings.py
@@ -0,0 +1,48 @@
+# String
+myStr = "Hello World"
+
+# Dir permite ver todo lo que se puede hacer con la variable
+print(dir(myStr))
+
+# Title y Upper
+
+print(myStr.title())  # Mayus en la primera letra
+
+print(myStr.upper())  # Todo en mayus
+print(myStr.lower())  # Todo en minus
+print(myStr.swapcase())  # Alterna mayus y minus
+print(myStr.capitalize())  # Vuelve mayus solo la primera letra del texto
+print(myStr.replace("Hello", "Goodbye"))  # Reemplaza una palabra por otra
+
+# Los metodos se pueden anidar
+print(myStr.replace("Hello", "Goodbye").upper())
+
+print(myStr.count("l"))  # Permite contar cuantas veces se repite el caracter
+
+print(myStr.startswith("Hola"))  # Permite consultar como arranca el string
+print(myStr.startswith("World"))  # Permite consultar como termina el string
+
+# Como dividir un string en una list
+print(myStr.split())  # Basado en los " ".
+print(myStr.split("l"))  # Tambien se puede separar por caracter
+
+# Buscar un caracter
+print(myStr.find("o"))  # Devuelve la POSICION del caracter
+# Para saber el tamaño completo del string
+print(len(myStr))
+# Buscar el indice de un caracter
+print(myStr.index("r"))
+# Para saber si es numerico o alfanumerico
+print(myStr.isnumeric())
+print(myStr.isalpha())
+
+# Consultar una posicion especifica
+print(myStr[7], myStr[3], myStr[4], myStr[8])
+# Con los numeros negativos puedo hacerlo de modo inverso
+print(myStr[-7], myStr[-2], myStr[-4], myStr[-3])
+
+# Final: Texto y string en un solo print
+print(myStr + "," + " my name is Gonzalo")
+# El f determina que voy a unar una variable que se pone entre {}
+print(f"{myStr}, my name is Gonzalo")
+print("{0}, my name is Gonzalo".format(myStr))
---
+++ variables.py
@@ -0,0 +1,22 @@
+# Variable = Nombre simbolico para un valor
+
+name = "Gonza"
+
+print(name)
+
+# Los nombres de las variables cambian por las mayusculas
+# Case sensitive
+book = "The lord of the rings: The fellowship of the ring"
+Book = "The lord of the rings: The two towers"
+BooK = "The lord of the rings: The return of the king"
+
+print(book)
+print(Book)
+print(BooK)
+
+# Conventions
+book_name = "The Silmarillion"  # Snake Case
+bookName = "The Silmarillion"  # Camel Case
+BookName = "The Silmarillion"  # Pascal Case
+
+PI = 3.1416  # Constantes todas en mayus
